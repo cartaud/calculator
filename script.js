0d04af5b67a1ee476df9f9395fbda04d3c11b822
Chadd <chadd_artaud@cox.net>
2022-04-27 15:20:00 -0700

    changed functions to help prevent bugs

--- script.js
+++ script.js
@@ -1,66 +1,66 @@
 let scrn = document.getElementById('value'); //will store current value of operation at top of screen
 let screenBot = document.getElementById('entering'); //display what user is entering
-let disp = [];
-let j = 0;
-
-
-// set up x(num before operator) and y(num after operator) values
-//set up an if else chain to figure what operator is and then x + y, x-y, x*y, ect...
-function enter(val) {
-    //when an integer button gets pressed, this function stores all the values in disp arr
-    let x = val/1
-    if (x == val && j <= 1) {
-        disp.push(val);
-        screenBot.textContent = disp.join('');
-    }
-    else if (x !== val  && j==0) {
-        disp.push(val);
-        screenBot.textContent = disp.join('');
-        j++;
-    }
-    else if (x !== val && j==1) {
-        let eq = disp.join('');
-        screenBot.textContent = '';
-        j--;
-        calculate(eq)
-    } 
-}
-
-
-function calculate(eq) {
-    if (eq.includes('+')) {
-        let index = eq.indexOf('+');
-        let x = parseInt(eq.slice(0,index))
-        let y = parseInt(eq.slice(index+1))
-        scrn.textContent = x + y;
-    }
-    else if (eq.includes('-')) {
-        let index = eq.indexOf('-');
-        let x = parseInt(eq.slice(0,index))
-        let y = parseInt(eq.slice(index+1))
-        scrn.textContent = x - y;
-    }
-    else if (eq.includes('*')) {
-        let index = eq.indexOf('*');
-        let x = parseInt(eq.slice(0,index))
-        let y = parseInt(eq.slice(index+1))
-        scrn.textContent = x * y;
-    }
-    else if (eq.includes('/')) {
-        let index = eq.indexOf('/');
-        let x = parseInt(eq.slice(0,index))
-        let y = parseInt(eq.slice(index+1))
-        scrn.textContent = x / y;
-    }
-    disp.length = 0;
-    enter(scrn.textContent)
-}
 
 let clearEl = document.querySelector('#clear');
 clearEl.addEventListener('click', clear)
 
+
+let first = [];
+let op = [];
+let second = []
+let j = 0;
+
+
+
+function enter(val) {
+    
+    if (op.length == 0 && val/1 == val && j == 0) {
+        first.push(val);
+        screenBot.textContent = first.join('');
+    }
+    else if (op.length == 0 && val/1 != val) {
+        op.push(val);
+        screenBot.textContent = first.join('') + op.join('');
+    }
+    else if (op.length != 0 && val/1 == val) {
+        second.push(val);
+        screenBot.textContent = first.join('') + op.join('') + second.join('');
+    }
+    else if (op.length != 0 && val/1 != val) {
+        calculate()
+    }
+}
+
+
+function calculate() {
+    let x = parseInt(first.join(''));
+    let y = parseInt(second.join(''));
+    if (op[0] == '+') {
+        scrn.textContent = x + y;
+    }
+    else if (op[0] == '-') {
+        scrn.textContent = x - y;
+    }
+    else if (op[0] == '*') {
+        scrn.textContent = x * y;
+    }
+    else if (op[0] == '/') {
+        scrn.textContent = x / y;
+    }
+    j++;
+    first.length = 0;
+    op.length = 0;
+    second.length = 0;
+    first[0] = scrn.textContent
+    enter(scrn.textContent)
+}
+
+
 function clear() {
-    disp.length = 0;
+    j = j-j;
+    first.length = 0;
+    op.length = 0;
+    second.length = 0;
     screenBot.innerHTML = ``;
     scrn.innerHTML = ``;
     //clear screen text too
